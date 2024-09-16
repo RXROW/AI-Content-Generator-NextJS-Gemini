c61798788205f23dd4e9c49a83fe0e29edb2c705
“Eslam“ <aslamalmswdya@gmail.com>
2024-09-17 01:18:23 +0300

    inital Comment

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+  <div className=" flex items-center flex-col">
+     AI Content Generator App Using NextJs
+     
+     <Button variant="outline">NextJs</Button>
+     
+     </div>
+);
+}
